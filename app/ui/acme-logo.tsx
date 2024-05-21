3b54cf0664bd276b70292e6fc63c0d2ffa27eece
cd.t <chasedemaster.tech@gmail.com>
2024-05-21 16:03:36 -0500

    skeleton load via streaming

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,8 @@ export default function AcmeLogo() {
       className={`${inter.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[24px]">KPI Dashboard</p>
+      <p className="text-[34px]">KPI</p>
+      
     </div>
   );
 }
